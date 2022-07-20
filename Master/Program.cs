93afc2e3453ea82dd45913b036d62b7271534d12
qalandardev <uzwebdevolopersn2@gamil.com>
2022-07-20 11:00:56 +0500

    Begin3

--- Master/Program.cs
+++ Master/Program.cs
@@ -6,11 +6,15 @@ namespace Master
     {
         static void Main(string[] args)
         {
-            /* Begin2◦. Дана сторона квадрата a. Найти его площадь S = a2. */
-            Console.WriteLine("Введите сторону квадрата a");
+            /* Begin3◦. Даны стороны прямоугольника a и b. Найти его площадь S = a·b и
+ периметр P = 2·(a + b). */
+            Console.WriteLine("Введите сторону прямоугольника a:");
             double a = Convert.ToDouble(Console.ReadLine());
-            double S = a * a;
-            Console.WriteLine("Площадь квадрата равна " + S);
+            Console.WriteLine("Введите сторону прямоугольника b:");
+            double b = Convert.ToDouble(Console.ReadLine());
+            double S = a * b;
+            double P = 2 * (a + b);
+            Console.WriteLine("Площадь прямоугольника S = {0} и периметр P = {1}", S, P);
             Console.ReadLine();
         }
     }
